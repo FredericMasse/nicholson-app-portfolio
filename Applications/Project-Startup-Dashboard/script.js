15381732087b4b72cf3e573e1e2f3e35fc7d9118
FredericMasse <frederic.masse@vinci-construction.com>
2026-09-20 21:27:26 -0400

    Add updateSummary function to display task completion

--- Applications/Project-Startup-Dashboard/script.js
+++ Applications/Project-Startup-Dashboard/script.js
@@ -1,1 +1,15 @@
+function updateSummary() {
 
+    const checkboxes =
+        document.querySelectorAll(".task");
+
+    const completed =
+        document.querySelectorAll(".task:checked");
+
+    document.getElementById("summary").innerHTML =
+        completed.length +
+        " of " +
+        checkboxes.length +
+        " tasks completed";
+
+}
